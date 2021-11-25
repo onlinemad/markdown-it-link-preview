d5acf6e7f11409066bbc0e114bcfb8ef90d2e3b6
Ian Wu <onlinemad@gmail.com>
2021-11-25 11:13:53 +0800

    Add User-Agent header in fetch request.

--- src/get_html_sync.js
+++ src/get_html_sync.js
@@ -1,7 +1,11 @@
 const fetch = require('sync-fetch')
 
 function getHtmlSync(url) {
-  const xhr = fetch(url)
+  const xhr = fetch(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53'
+    }
+  })
   if (xhr.status !== 200) {
     const statusText = xhr.statusText ? xhr.statusText : '';
     throw new Error(
